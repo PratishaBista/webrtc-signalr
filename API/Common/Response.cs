2ee67ec68d622138a8e7ca36417526fe2d60e6cb
PratishaBista <bista.pratisha@gmail.com>
2025-06-18 14:27:40 +0545

    Create Register Endpoint using minimal API

---
+++ API/Common/Response.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace API.Common;
+
+public class Response<T>
+{
+    public bool IsSuccess { get; set; }
+
+    public T Data { get; set; }
+    public string? Error { get; set; }
+
+    public string? Message { get; set; }
+
+
+    public Response(bool isSuccess, T data, string? error = null, string? message = null)
+    {
+        IsSuccess = isSuccess;
+        Data = data;
+        Error = error;
+        Message = message;
+    }
+
+    public static Response<T> Success(T data, string? message = null) => new(true, data, null, message);
+    public static Response<T> Failure(string error) => new(false, default!, error, null);
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,48 @@
 using API.Data;
+using API.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var JwtSettings = builder.Configuration.GetSection("JwtSetttings");
 builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("Data Source=chat.db"));
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddIdentityCore<AppUser>()
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+        (JwtSettings.GetSection("SecretKey").Value!)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+
+});
 builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -19,5 +51,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapAccountEndpoint();
 
 app.Run();
