ad09467fdb2ef157a957a49a49cae4e532ed0e61
PratishaBista <bista.pratisha@gmail.com>
2025-07-11 12:47:10 +0545

    Profile upload api created

--- API/Endpoints/AccountEndpoint.cs
+++ API/Endpoints/AccountEndpoint.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Common;
 using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,9 +13,11 @@ public static class AccountEndpoint
     {
         var group = app.MapGroup("/api/account").WithTags("account");
 
-        group.MapPost("/register", async (HttpContext httpContext, UserManager<AppUser>
+        group.MapPost("/register", async (HttpContext context, UserManager<AppUser>
         userManager, [FromForm] string fullname, [FromForm] string email,
-        [FromForm] string password, [FromForm] string username) =>
+        [FromForm] string password, [FromForm] string username, [FromForm] IFormFile? profileImage) =>
+
+        //IFormFile is nullable to allow registration without a profile image
         {
             var userFromDb = await userManager.FindByEmailAsync(email);
             if (userFromDb is not null)
@@ -22,11 +25,22 @@ public static class AccountEndpoint
                 return Results.BadRequest(Response<string>.Failure("User already exists"));
             }
 
+            // Validate the profile image if provided
+            if (profileImage is null)
+            {
+                return Results.BadRequest(Response<string>.Failure("Profile image is required"));
+            }
+
+            var picture = await FileUpload.Upload(profileImage);
+
+            picture = $"{context.Request.Scheme}://{context.Request.Host}/uploads/{picture}";
+
             var user = new AppUser
             {
                 Email = email,
                 FullName = fullname,
                 UserName = username,
+                ProfileImage = picture
             };
 
             var result = await userManager.CreateAsync(user, password);
--- API/Program.cs
+++ API/Program.cs
@@ -54,6 +54,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseStaticFiles();
 
 app.MapAccountEndpoint();
 app.MapGet("/", () => "API is running!").AllowAnonymous();
