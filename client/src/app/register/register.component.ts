ded86d6c9c5da934a7d842648ce3161b9e025399
PratishaBista <bista.pratisha@gmail.com>
2025-07-25 23:09:44 +0545

    Create registration page

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -12,7 +16,7 @@ export class RegisterComponent {
   password!: string;
   fullName!: string;
   username!: string;
-  profilePicture!: string;
+  profilePicture: string = "https://randomuser.me/api/portraits/lego/1.jpg";
   profileImage: File | null = null;
   authService = inject(AuthServiceService);
 }
